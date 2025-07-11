cd5e96a200e70fefaee970e8ecc2e3073369c900
VirajParmar96 <viraj.parmar960@gmail.com>
2025-07-11 18:33:34 +0530

    1. Hello World Program

---
+++ P01_HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+  Program: Hello World
+  Level  : Basic
+  Description:
+    A simple Java program to print "Hello, World!" on the screen. 
+*/
+
+public class P01_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+/*
+  Sample Output:
+  Hello, World!
+*/
