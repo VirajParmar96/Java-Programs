c55ef3729e8e57c4066f338d2feead541ed9e184
VirajParmar96 <viraj.parmar960@gmail.com>
2025-07-12 19:59:41 +0530

    5. Check whether the Number is Even or Odd

---
+++ P05_EvenOrOdd.java
@@ -0,0 +1,35 @@
+/*
+  Program: Check Even or Odd
+  Level  : Basic
+  Description:
+    This program checks whether a given integer is even or odd.
+*/
+
+import java.util.Scanner;
+
+public class P05_EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even.");
+        } else {
+            System.out.println(number + " is Odd.");
+        }
+        scanner.close();
+    }
+}
+
+/*
+  Sample Output:
+  Enter an integer: 7
+  7 is Odd.
+
+  OR
+
+  Enter an integer: 12
+  12 is Even.
+*/
